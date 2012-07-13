7e3dd274b42b8542312017df5342fd03eee30e4d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-07-13 14:47:54 +0100

    Fix building libmemalloc without ION
    Change-Id: Ia24f8f6d3b7a66a2b410d1684eb64951da1dfc79

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -38,6 +38,14 @@ LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libgenlock
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdmemalloc\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
-LOCAL_SRC_FILES               :=  ionalloc.cpp alloc_controller.cpp
+LOCAL_SRC_FILES               :=  alloc_controller.cpp
+ifeq ($(TARGET_USES_ION),true)
+    LOCAL_SRC_FILES += ionalloc.cpp
+    LOCAL_CFLAGS += -DUSE_ION
+else
+    LOCAL_SRC_FILES += ashmemalloc.cpp \
+                    pmemalloc.cpp \
+                    pmem_bestfit_alloc.cpp
+endif
 
 include $(BUILD_SHARED_LIBRARY)
