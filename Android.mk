95becebbaf51422962c2b60a968eabae1363bc6a
nikez <nikez141@gmail.com>
2013-01-31 01:16:39 +0100

    display-legacy: Use TARGET_QCOM_DISPLAY_VARIANT flag for usage.
    Change-Id: Ia348226eeb7bda8368edac4a7086ed4413a6597d

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 #Enables the listed display HAL modules
 #libs to be built for QCOM targets only
 
-ifeq ($(BOARD_USES_LEGACY_QCOM),true)
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
