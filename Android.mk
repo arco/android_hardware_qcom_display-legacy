03df126549da6c1d4d4b6fcf0fbdb7248858944f
Arne Coucheron <arco68@gmail.com>
2013-03-23 01:05:03 +0100

    Change makefiles for qcom legacy
    Change-Id: I0e70a52ce020ba107b5cb0f19746100c6766e76d

--- Android.mk
+++ Android.mk
@@ -1,16 +1,10 @@
-#Enables the listed display HAL modules
-#libs to be built for QCOM targets only
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
-
-ifneq ($(TARGET_PROVIDES_LIBLIGHTS),true)
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
-
 endif
-
-display-hals += libtilerenderer
-
 include $(call all-named-subdir-makefiles,$(display-hals))
+endif
