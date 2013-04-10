6c0c866de21da9693a1ecc485f7822b8b2c44307
Arne Coucheron <arco68@gmail.com>
2013-04-10 05:50:27 +0200

    Update and clean makefiles for building in CM-10.1
    Change-Id: I6c42b99b86f7ebb30961f73c0f4ba74ab510c957

--- common.mk
+++ common.mk
@@ -1,20 +1,19 @@
 #Common headers
-common_includes := hardware/qcom/display/libgralloc
-common_includes += hardware/qcom/display/libgenlock
-common_includes += hardware/qcom/display/liboverlay
-common_includes += hardware/qcom/display/libcopybit
-common_includes += hardware/qcom/display/libqdutils
-common_includes += hardware/qcom/display/libhwcomposer
-common_includes += hardware/qcom/display/libexternal
-common_includes += hardware/qcom/display/libqservice
+common_includes := hardware/qcom/display-legacy/libgralloc
+common_includes += hardware/qcom/display-legacy/libgenlock
+common_includes += hardware/qcom/display-legacy/liboverlay
+common_includes += hardware/qcom/display-legacy/libcopybit
+common_includes += hardware/qcom/display-legacy/libqdutils
+common_includes += hardware/qcom/display-legacy/libhwcomposer
+common_includes += hardware/qcom/display-legacy/libexternal
+common_includes += hardware/qcom/display-legacy/libqservice
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_flags     += -DUSES_POST_PROCESSING
     common_includes += $(TARGET_OUT_HEADERS)/pp/inc
 endif
 
-
-#Common libraries external to display HAL
+#Common libraries external to display-legacy HAL
 common_libs := liblog libutils libcutils libhardware
 
 #Common C flags
@@ -27,6 +26,10 @@ ifeq ($(TARGET_BOARD_PLATFORM), msm8960)
     common_flags += -DUSE_FENCE_SYNC
 endif
 
+ifeq ($(TARGET_USES_ION),true)
+    common_flags += -DUSE_ION
+endif
+
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
 endif
@@ -37,13 +40,4 @@ endif
 
 ifeq ($(TARGET_QCOM_HDMI_RESOLUTION_AUTO),true)
     common_flags += -DFORCE_AUTO_RESOLUTION
-endif
-
-common_deps  :=
-kernel_includes :=
-
-#Kernel includes. Not being executed on JB+
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
