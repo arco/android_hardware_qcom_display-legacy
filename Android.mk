CyanogenMod/android_hardware_qcom_display

qcom: add ifdef to liblight
Change-Id: I0dd430a53c3e16083251f6ecf916be8142a4b732

Android.mk
@@ -2,8 +2,13 @@
 #libs to be built for QCOM targets only
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHTS),true)
+display-hals += liblight
+endif
+
 endif
 
 include $(call all-named-subdir-makefiles,$(display-hals))

arco/android_hardware_qcom_display-legacy

Android.mk
@@ -1,7 +1,11 @@
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
 
 include $(call all-named-subdir-makefiles,$(display-hals))
 endif
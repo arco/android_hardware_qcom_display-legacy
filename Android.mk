f4b5208267c84ca530264896baa78066e6e76c16
Shareef Ali <shareefalis@gmail.com>
2012-07-13 13:17:40 -0500

    qcom: add ifdef to liblight
    Change-Id: I0dd430a53c3e16083251f6ecf916be8142a4b732

--- Android.mk
+++ Android.mk
@@ -1,7 +1,11 @@
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
 
 include $(call all-named-subdir-makefiles,$(display-hals))
 endif
