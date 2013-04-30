4e10ca537aee0fe4e716d1a4c4b460e0cea1bb3d
Arne Coucheron <arco68@gmail.com>
2013-04-30 10:36:10 +0200

    Allow usage of kernel headers
    Change-Id: I9fa11bd72fa632551679d8b13a89cccbaae53e77

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay libgenlock \
                                  libhwcexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libhwcservice
